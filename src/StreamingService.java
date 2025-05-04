8461aaffff5e8b4e689dcabfa85c09a9d48d1dac
abdul <abdul@8bit>
2025-05-04 03:17:15 +0300

    Updated

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -1,4 +1,34 @@
 
+import java.util.List;
+
+
 public class StreamingService {
     
+    private List<Content> catalog;
+    
+    
+    public List<Content> searchContent(String title){
+        return null;
+    }
+           
+        
+        
+    public void streamContent(String contentId){
+        
+    } 
+    
+    
+    public List<Content> getCatalog(){
+        return catalog;
+    }
+    
+    public void setCatalog(List<Content> catalog){
+        this.catalog = catalog;
+        
+    }
+    
+    
+    
+    
 }
+
